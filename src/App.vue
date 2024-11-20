45f2ba1fb2650244eca5baaf407b0fa3366076c4
Xerithh <157456613+Xerithh@users.noreply.github.com>
2024-11-20 15:27:59 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div ref="box" class="w-20 h-20 bg-blue-500"></div>
+  <h1>Salut</h1>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'
+import { gsap } from 'gsap'
+
+export default {
+  setup() {
+    const box = ref(null)
+
+    onMounted(() => {
+      gsap.to(box.value, { x: 200, duration: 2 })
+    })
+
+    return { box }
+  },
+}
+</script>
